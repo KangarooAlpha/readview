f34b74425e3641a9326fcc1ef176cfd027604126
Omar <omar.fayyaz@ryerson.ca>
2025-07-31 19:20:42 -0400

    added user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, uniqueness: true
+  has_one_attached :avatar
 
   has_many :posts
   has_many :post_likings
