50f4ce623cbde66d7c088659f5ef45d927749741
Omar <omar.fayyaz@ryerson.ca>
2025-08-12 16:39:48 -0400

    added image previews

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def post_preview_image(post)
+    image = post.contents
+      .select { |c| c.contentable.is_a?(ImageContent) }
+      .flat_map { |c| c.contentable.images }
+      .first
+
+    image if image.present?
+  end
 end
